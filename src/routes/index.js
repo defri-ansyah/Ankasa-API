ae030ba043ff9e389862b5d98694784285d8ad21
Defri Ansyah <defriansyah013@gmail.com>
2021-01-08 02:15:06 +0700

    init user

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('./auth')
+const users = require('./users')
 const airLines = require('./airlines')
 
 router.use('/auth', auth)
+router.use('/user', users)
 router.use('/airlines', airLines)
 
 module.exports = router
