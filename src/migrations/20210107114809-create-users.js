3a7b2c538b96092fd1da364950e9c91738a46cc2
Defri Ansyah <defriansyah013@gmail.com>
2021-01-07 19:09:57 +0700

    create auth

---
+++ src/migrations/20210107114809-create-users.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      phone_number: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      post_code: {
+        type: Sequelize.INTEGER
+      },
+      role_id: {
+        type: Sequelize.INTEGER
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-
+const auth = require('./auth')
 const airLines = require('./airlines')
 
+router.use('/auth', auth)
 router.use('/airlines', airLines)
 
 module.exports = router
