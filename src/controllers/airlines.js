3223192d53637c3ed2d6827364aa1a0cf9cd8d88
Gefyaqiilah <gefyaqiilah26@gmail.com>
2021-01-10 17:13:53 +0700

    revisi baseurl

--- src/controllers/airlines.js
+++ src/controllers/airlines.js
@@ -14,7 +14,7 @@ const controllers = {
     }
     AirLines.create({
       name: name,
-      logo: `${process.env.BASE_URL}/images/${req.file.filename}`
+      logo: `${process.env.BASE_URL}images/${req.file.filename}`
     })
     .then(() => {
       response(res, 'airlines has been added', { status: 'success', statusCode:200 }, null )
